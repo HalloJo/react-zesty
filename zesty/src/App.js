d3c97b8e3a134a20afa6652bb85a0f5645147392
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-03-11 16:34:08 +0100

    Add images and click event with handleClick function

--- zesty/src/App.js
+++ zesty/src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avocado from "./assets/image6.jpeg";
 
-const images = [];
+const images = [cabbage, mango, fig, gaze, peach, avocado];
 
 const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
   return (
     <div className="App">
       <div className="title">
@@ -13,7 +28,7 @@ const App = () => {
         </p>
       </div>
       <div className="image-container">
-        <img alt="" src="" />
+        <img alt="" src={images[currentImage]} onClick={handleClick} />
       </div>
     </div>
   );
