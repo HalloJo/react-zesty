82a2fcb883b3f4c9dfb5fb5148037757f714995a
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-03-11 16:08:56 +0100

    Set up title element and image element, add google font

--- zesty/src/App.js
+++ zesty/src/App.js
@@ -1,15 +1,22 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
+const images = [];
+
+const App = () => {
   return (
     <div className="App">
-      <h1>Zesty</h1>
-      <p>
-        A photography project <br /> by Ella Fieldling
-      </p>
+      <div className="title">
+        <h1>Zesty</h1>
+        <p>
+          A photography project <br /> by Ella Fieldling
+        </p>
+      </div>
+      <div className="image-container">
+        <img alt="" src="" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
